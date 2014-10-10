87d6a6f1d493ec8b454141b200b146f67ce15c12
Junjie Guan <gjj2684@gmail.com>
2014-10-09 21:13:36 -0400

    Add basic linked list operations

---
+++ list.c
@@ -0,0 +1,161 @@
+#include "standardLib.h"
+#include "stdlib.h"     // For the use of malloc, will be replace with yalnix malloc 
+
+int list_init(list_t *list) {
+    list = (list_t*)malloc(sizeof(list_t));
+    if(list) {
+        list->size = 0; 
+        list->rc = 0; 
+        return 0;
+    } else {
+        list->rc = ERR_NULL_POINTER; 
+        return ERR_NULL_POINTER;
+    }
+}
+
+int list_clear(list_t *list) {
+    if(!list) {
+        list->rc = ERR_NULL_POINTER; 
+        return ERR_NULL_POINTER;
+    }
+    node_t *p = list->head, *prev;
+    while(p != NULL) {
+        prev = p;
+        p = p->next;
+        free(prev);
+    }
+    list->size = 0;
+    return 0;
+}
+
+void list_deconstruct(list_t *list) {
+    list->rc = list_clear(list);
+    if(list->rc) {
+        free(list);
+        list = NULL;
+        return 0;
+    } else {
+        return list->rc;
+    }
+}
+
+int list_pushFront(list_t *list, node_t *node) {
+    if(!list) {
+        list->rc = ERR_NULL_POINTER; 
+        return ERR_NULL_POINTER;
+    }
+    node->next = list->head;
+    list->head = node;
+    list->size++;
+    return 0;
+}
+
+
+int list_pushRear(list_t *list, node_t *node) {
+    if(!list) {
+        list->rc = ERR_NULL_POINTER; 
+        return ERR_NULL_POINTER;
+    }
+    if(list->rear) {
+        list->rear->next = node;
+    } 
+    node->next = NULL;
+    list->rear = node;
+    list->size++;
+    return 0;
+}
+
+node_t* list_findIdx(list_t *list, int idx) {
+    if(!list) {
+        list->rc = ERR_NULL_POINTER;
+        return ERR_NULL_POINTER;
+    }
+    if(idx >= list->size || idx < 0) {
+        list->rc = ERR_OUT_OF_RANGE; 
+        return ERR_OUT_OF_RANGE;
+    }
+
+    int i = -1;
+    node_t *node = NULL;
+    while(i < idx) {
+        if(!node) {
+            node = list->head;
+        } else {
+            node = node->next;
+        }
+        i++;
+    }
+    return node;
+}
+
+int list_insert(list_t *list, node_t *node, int idx) {
+    if(idx == 0) {
+        return list_pushFront(list, node);        
+    }
+    if(idx == list->size) {
+        return list_pushRear(list, node);
+    }
+
+    node_t *prev = list_findIdx(list, idx - 1);
+    if(!node) {
+        return list->rc;
+    }
+    node->next = prev->next;
+    prev->next = node;
+    return 0;
+}
+
+node_t* list_popFront(list_t *list) {
+    if(!list || !list->size) {
+        list->rc = ERR_NULL_POINTER;
+        return ERR_NULL_POINTER;
+    }
+    node_t *node = list->head;
+    list->head = list->head->next;
+    list->size--;
+    if(list->size == 0) {
+        list->tail = NULL;
+    }
+    node->next = NULL;
+    return node;
+}
+
+/* Noted that, list_rmIdx do not free the memory of the removed node!
+ */
+node_t* list_rmIdx(list_t *list, int idx) {
+    if(!list) {
+        list->rc = ERR_NULL_POINTER;
+        return NULL;
+    }
+    if(idx >= list->size || idx < 0) {
+        list->rc = ERR_OUT_OF_RANGE; 
+        return NULL;
+    }
+
+    if(idx = 0) {
+        return list_popFront(list);
+    } else {
+        node_t* prev = NULL;
+        int i = -1;
+        while(i < idx - 1) {
+            if(!prev) {
+                prev = list->head;
+            } else {
+                prev = prev->next;
+            }
+            i++;
+        }
+        node_t *rmNode = prev->next;
+        prev->next = rmNode->next;
+        rmNode->next = NULL;
+        list->size--;
+        return rmNode;
+    }
+}
+
+node_t* list_popRear(list_t *list) {
+    return list_rmIdx(list, list->size - 1);
+}
+
+
+
--- standardLib.h
+++ standardLib.h
@@ -1,20 +1,56 @@
+
 /* Some useful library for both user and kernal
  */
+#ifndef STANDARD_LIB
+#define STANDARD_LIB
+
+#define ERR_NULL_POINTER    1
+#define ERR_OUT_OF_RANGE    2
+#define ERR_NON_KNOWN       100
+
+typedef struct y_Node {
+    void            *value;
+    struct y_Node   *next;
+} node_t;
+
+typedef struct y_DoublyNode {
+    void            *value;
+    struct y_Node   *next;
+    struct y_Node   *prev;
+} dnode_t;
+
 typedef struct y_List {
-    
+    int         size;
+    int         rc;
+    node_t      *front;    
+    node_t      *rear;    
 } list_t;
 
-typedef struct y_Stack {
-
-} stack_t;
-
-typedef struct y_Queue {
-
-} queue_t;
+typedef struct y_DoublyList {
+    int         size;
+    int         rc;
+    dnode_t     *front;    
+    dnode_t     *rear;    
+} dlist_t;
 
 typedef struct y_Vector {
-
+    int         size;
+    int         rc;
+    void        *arr;
 } vector_t;
+
+/* We don't need an explicit stack or queue, 
+ * we can simply use vector or list as stack or queue
+ */
+//typedef struct y_Stack {
+//    int         size;
+//    void        *top;
+//} stack_t;
+//
+//typedef struct y_Queue {
+//    
+//} queue_t;
+
 
 typedef struct y_Heap {
 
@@ -27,4 +63,8 @@ typedef struct y_Hashset {
 typedef struct y_HashMap {
 
 } hashmap_t;
+
+void *malloc(size_t size);
+void free(void *ptr);
+#endif
 
