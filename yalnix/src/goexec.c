2bf0c693d905edc7f84ded1966e62677b2db9aab
Junjie Guan <gjj2684@gmail.com>
2014-10-30 18:41:38 -0400

    Build test case for Wait and Exit

--- yalnix/src/goexec.c
+++ yalnix/src/goexec.c
@@ -1,7 +1,8 @@
 void main(void) {
-    int cnt = 25;
+    int cnt = 20;
     while(cnt) {
         TracePrintf(5, "GJJ is doing EXEC like a boss, with %d remaining loops\n", cnt--);
         Pause();
     }
+    Exit(42);
 }
--- yalnix/src/init.c
+++ yalnix/src/init.c
@@ -4,6 +4,7 @@ void main(void) {
     int delay = 5;
     int delays[512];
     int delays2[1024];
+    int exit_status;
     int* a;
     a = (int*)malloc(sizeof(int) * 100);
     int cnt = 0;
@@ -17,8 +18,10 @@ void main(void) {
     
     user_log("After fork come with pid %d", pid);
     if(pid != 0) {
+        user_log("I am parent with PID(%d)", GetPid());
+        int cpid = Wait(&exit_status);
+        user_log("Wait my child(%d) done, return meaning of life %d", cpid, exit_status);
         while(1) {
-            user_log("I am parent with PID(%d)", GetPid());
             //Delay(3);
             //user_log("PID(%d) back from delay", GetPid());
             Delay(3);
